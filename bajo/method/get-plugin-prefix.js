9a94c10de5596a3d23ba0310c50febf54d9dad8b
Ardhi Lukianto <ardhi@lukianto.com>
2024-11-03 11:09:15 +0700

    Updates

--- bajo/method/get-plugin-prefix.js
+++ bajo/method/get-plugin-prefix.js
@@ -1,7 +1,7 @@
-function getAppPrefix (base) {
+function getPluginPrefix (base) {
   const { get, trim } = this.app.bajo.lib._
   const prefix = get(this, `app.${base}.config.waibu.prefix`, this.app[base].alias)
   return trim(prefix, '/')
 }
 
-export default getAppPrefix
+export default getPluginPrefix
