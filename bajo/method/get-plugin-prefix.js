ced9bd0c458518addd54534a3748d699958a36ec
Ardhi Lukianto <ardhi@lukianto.com>
2025-02-17 22:05:48 +0700

    Updates

--- bajo/method/get-plugin-prefix.js
+++ bajo/method/get-plugin-prefix.js
@@ -1,6 +1,11 @@
-function getPluginPrefix (base) {
+function getPluginPrefix (base, webApp = 'waibuMpa') {
   const { get, trim } = this.app.bajo.lib._
-  const prefix = get(this, `app.${base}.config.waibu.prefix`, this.app[base].alias)
+  let prefix = get(this, `app.${base}.config.waibu.prefix`, this.app[base].alias)
+  if (base === 'main') {
+    const cfg = this.app[webApp].config
+    if (cfg.mountMainAsRoot) prefix = ''
+  }
+
   return trim(prefix, '/')
 }
 
